0af57a92095046cb96e472fdc55cd689ec5c276a
TANONGSAK SUKYAREAK <61729940+6210210599@users.noreply.github.com>
2020-03-03 15:06:18 +0700

    Add footer file

---
+++ miniservice/footer.ini.php
@@ -0,0 +1,13 @@
+<table style="width:100%" border="0px">
+		<tr>
+			<td><center>
+            MINI SERVICE<br>
+    HATYAI SONGKHLA 90110<br>
+    TELL : 084-999-9999<br>
+    E-MAIL : MINISERVICE@gmail.com
+			</td>
+		</tr>
+</table>
+
+</body>
+</html>
